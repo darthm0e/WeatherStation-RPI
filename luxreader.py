9cb87b74188b8e18963cdaa36c2c58558185b91d
darthm0e <supamoe25@gmail.com>
2015-10-27 15:43:16 +0100

    geändert:               luxreader.py

--- luxreader.py
+++ luxreader.py
@@ -5,7 +5,7 @@ class TSL2561:
 
     i2c = None
 
-    def __init__(self, address=0x39, debug=0, pause=0.8):
+    def __init__(self, address=0x29, debug=0, pause=0.8):
         self.i2c = Adafruit_I2C(address)
         self.address = address
         self.pause = pause
